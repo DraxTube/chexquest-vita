b0e8fadf612dfcb1c78d43ee762133e0f83857df
DraxTube01 <draxtube01@gmail.com>
2026-02-22 19:57:35 +0100

    Create m_fixed.h

---
+++ src/m_fixed.h
@@ -0,0 +1,11 @@
+#ifndef __M_FIXED__
+#define __M_FIXED__
+
+#include "doomtype.h"
+#include "doomdef.h"
+
+fixed_t FixedMul(fixed_t a, fixed_t b);
+fixed_t FixedDiv(fixed_t a, fixed_t b);
+fixed_t FixedDiv2(fixed_t a, fixed_t b);
+
+#endif
